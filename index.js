d13b849fad643d9e0538d3c897f312d20cce257e
HeleneSoubeyrand <helenesoubi@gmail.com>
2021-10-20 12:34:51 +0200

    corrections function display, shuffle et submit

--- index.js
+++ index.js
@@ -8,14 +8,33 @@ $(function() {
         // fonction .forEach directement dans ajax // 
         // data.forEach(function(character) // 
             arrayCharacters = data
-        
-        displayCharacters(data)
+            displayCharacters(data)
         }
     })
+
+        // fonction recherche // 
+        // action "submit" // 
+    $("form").submit(function(input) {
+    // .preventDefault permet d'empêcher la réactualisation de toute la page // 
+        input.preventDefault()
+
+    // créer une valeur pour l'entrée de l'input // 
+        var value = $("input").val()
+     // créer une variable pour le nouveau tableau créé par filter // 
+        var search = arrayCharacters.filter(function(character) {
+     // comparer la valeur recherchée et la retourner // 
+        return character.fullName.toLowerCase().includes(value)
+        })
+
+     // afficher le nouveau tableau créée par filter  // 
+        displayCharacters(search)
+    })
+
 
     // fonction affichage // 
     function displayCharacters(arrayCharacters) {
         $(".grid").html("")
+
         arrayCharacters.forEach(function(character) {
             $(".grid").append(`
             <div class="col-md-4 col-sm-6 col-12">
@@ -28,26 +47,12 @@ $(function() {
             </div>
             `)
         })
+
     }
 
-
-    // fonction recherche // 
-        // action "submit" // 
-   $("form").submit(function(input) {
-       // .preventDefault permet d'empêcher la réactualisation de toute la page // 
-       input.preventDefault()
-       // créer une valeur pour l'entrée de l'input // 
-    var value = $("input").val()
-        // créer une variable pour le nouveau tableau créé par filter // 
-    var search = arrayCharacters.filter(function(character) {
-        // comparer la valeur recherchée et la retourner // 
-    return character.fullName.toLowerCase().includes(value)
+    // fonction shuffle // 
+    $(".shuffle").click(function() {
+        var shuffleCharacters =
+        displayCharacters(shuffleCharacters)
     })
-        // afficher le nouveau tableau créée par filter  // 
-    displayCharacters(search)
-   })
-   
-
-   // fonction random // 
-
 })  
