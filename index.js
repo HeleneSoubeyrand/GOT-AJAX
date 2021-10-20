0065ac3c916a27d6bc75dae951a251fa25baf8e2
HeleneSoubeyrand <helenesoubi@gmail.com>
2021-10-20 11:51:39 +0200

    function recherche

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
 $(function() {
+    var arrayCharacters = []
 
     // fonction données // 
     $.ajax({
         url: 'https://thronesapi.com/api/v2/Characters',
-        success: function(data) {  
-        displayCharacter(data)
+        success: function(data) { 
+        // fonction .forEach directement dans ajax // 
+        // data.forEach(function(character) // 
+            arrayCharacters = data
+        
+        displayCharacters(data)
         }
     })
 
     // fonction affichage // 
-    function displayCharacter(data) {
-        data.forEach(function(character) {
+    function displayCharacters(arrayCharacters) {
+        $(".grid").html("")
+        arrayCharacters.forEach(function(character) {
             $(".grid").append(`
-            <div class="card col-3 m-2">
+            <div class="col-md-4 col-sm-6 col-12">
+                <div class="card"> 
                     <div class="image" style="background-image: url('${character.imageUrl}');">
                     </div>
                     <h2 class="fullName text-center">${character.fullName}</h2>
                     <p class="title text-center">${character.title}</p>
+                </div>
             </div>
-
             `)
         })
     }
 
+
     // fonction recherche // 
-    function submit() {}
+        // action "submit" // 
+   $("form").submit(function(input) {
+       // .preventDefault permet d'empêcher la réactualisation de toute la page // 
+       input.preventDefault()
+       // créer une valeur pour l'entrée de l'input // 
+    var value = $("input").val()
+        // créer une variable pour le nouveau tableau créé par filter // 
+    var search = arrayCharacters.filter(function(character) {
+        // comparer la valeur recherchée et la retourner // 
+    return character.fullName.toLowerCase().includes(value)
+    })
+        // afficher le nouveau tableau créée par filter  // 
+    displayCharacters(search)
+   })
+   
+
+   // fonction random // 
 
 })  
